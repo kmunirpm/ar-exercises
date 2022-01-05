937a23cc0d92455f053f7503918485fb747f18da
kmunirpm <km092009@gmail.com>
2022-01-05 20:54:15 +0000

    exercise 2: update the 1st store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+store1_update = Store.find_by(id:1)
+store1_update = @store1
+
+store2_update = Store.find_by(id:2)
+store2_update = @store2
+
+store1_update2 = Store.find_by(@store1)
+store1_update2.name = "Coquitlam"
+store1_update2.save
