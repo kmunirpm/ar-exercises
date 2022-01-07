d6130fbd2388cb9f9eb749dbe9fdbb33d4d72c91
kmunirpm <km092009@gmail.com>
2022-01-07 16:11:46 +0000

    exercise 5 - Sums / averages

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "The total revenue is #{Store.sum(:annual_revenue)}"
+puts "The average annual revenue is $#{Store.average(:annual_revenue)}"
+puts "Count store >10000000 $#{Store.where("annual_revenue > 1000000").count}"
+
