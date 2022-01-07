f1acd27502cc91539d0f7c15c32bb441c845c695
kmunirpm <km092009@gmail.com>
2022-01-07 16:49:39 +0000

    exercise 7 - Employees and stores validations applied

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,29 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Store Count: #{Store.all.count}"
+
+store7 = Store.create(
+  name: "Both False", 
+  annual_revenue: 224000, 
+  mens_apparel: false, 
+  womens_apparel: false
+)
+store8 = Store.create(
+  name: "Only one False",
+  annual_revenue: 224000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+puts "Store Count: #{Store.all.count}"
+
+puts "please name a store"
+name = gets.chomp
+
+store9 = Store.create(name: name)
+
+store9.errors.each do |field, error|
+    puts "#{field} #{error}"
+end
+
+puts "Store Count: #{Store.all.count}"
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true , greater_than: 0 }
+  validate :men_or_womens
+
+  def men_or_womens
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:apparel, "Sorry! Stores must carry at least one of the men's or women's apparel.")
+    end
+  end
 end
